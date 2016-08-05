5ea8cd3d4922ff3331f815942617941a539430a1
Andrew Silluron <asilluron@users.noreply.github.com>
2016-08-04 20:35:34 -0700

    Bluebird promises for queries. Fixed raced condition on mongoose connection.

--- Service.js
+++ Service.js
@@ -5,14 +5,22 @@ var EventEmitter = require('events').EventEmitter;
 let Connector = require('./lib/Connector');
 
 class Service extends EventEmitter {
-  constructor (mongoUrl, rabbitUrl, queueConfigs) {
+  constructor (mongoUrl, rabbitUrl) {
     super();
-    this.qConfigs = queueConfigs || [];
+    this.qConfigs = [];
     this.queues = 0;
+    this.namedQueues = new Map(); // Exchange name > exchange
     this.connection = new Connector(mongoUrl, rabbitUrl);
     this.mongoose = this.connection.getMongooseReference();
     this.connection.once('ready', this._onConnected.bind(this));
     this.connection.once('lost', this._onLost.bind(this));
+  }
+
+  addQueues (queueConfigs) {
+    if (!Array.isArray(queueConfigs)) {
+      throw new Error('queueConfigs must be an array of queue config objects');
+    }
+    this.qConfigs = queueConfigs;
   }
 
   getMongooseReference () {
@@ -21,17 +29,21 @@ class Service extends EventEmitter {
 
   start () {
     this.qConfigs.forEach(queueConfig => {
-      this.connection.queue.handle(queueConfig.name, queueConfig.handler);
+      let queue = this.namedQueues.get(queueConfig.name);
+      queue.consume(queueConfig.handler);
     });
   }
 
   publish (queueName, payload) {
-    this.connection.queue.publish(queueName, payload);
+    this.connection.exchange.publish(payload, { key: queueName });
   }
 
   _onConnected () {
     this.qConfigs.forEach(qConfig => {
-      this.connections.queue.create(qConfig.name, { prefetch: qConfig.prefetch }, this._onCreate);
+      // TODO: make this more programmatic, so we just shave off handler and feed in the whole config
+      let newQueue = this.connection.exchange.queue({name: qConfig.name, durable: true});
+      this.namedQueues.set(qConfig.name, newQueue);
+      this._onCreate();
     });
   }
 
